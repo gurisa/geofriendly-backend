edc23e2f15cfe2bfa2462a6d035fa9661d048660
Raka Suryaardi Widjaja <raka.suryadi@gmail.com>
2018-09-09 18:13:34 +0700

    [ADD] basic restplus schema

--- main.py
+++ main.py
@@ -6,87 +6,247 @@ from flask_restplus import Resource, Api
 # from model import Users
 
 app = Flask(__name__)
-api = Api(app, version='0.1', title='GeoFriendly', description='Visit for more: <a href="https://www.gurisa.com" target="_blank">Gurisa Devs</a>')
+api = Api(app, version='0.1', doc='/doc/', default ='Endpoint', default_label='List of api endpoint', title='GeoFriendly', description='More stuffs: <a href="https://www.gurisa.com" target="_blank">Gurisa Devs</a>')
 
-@api.route('/users')
-class Users(Resource):
+class UserListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/class')
-class Class(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class UserApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class ClassListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/families')
-class Families(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class ClassApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class FamilyListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/ages')
-class Ages(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class FamilyApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class AgeListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/acquisitions')
-class Acquisitions(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class AgeApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class AcquisitionListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/specimens')
-class Specimens(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class AcquisitionApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class SpecimenListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/maps')
-class Maps(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class SpecimenApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class MapListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/collections')
-class Collections(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class MapApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class CollectionListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/acquisitions')
-class Acquisitions(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class CollectionApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class ScaleListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/scales')
-class Scales(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class ScaleApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class IslandListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/islands')
-class Islands(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class IslandApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class RackListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/racks')
-class Racks(Resource):
+    def post(self):
+        return {'hello': 'world'}
+
+class RackApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
+
+    def put(self, id):
+        return {'hello': 'world'}
+
+    def delete(self, id):
+        return {'hello': 'world'}
+
+class DrawerListApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
-@api.route('/drawers')
-class Drawers(Resource):
-    def get(self):
+    def post(self):
         return {'hello': 'world'}
 
-#     model = api.model('Model', {
-#         'id': id.Integer,
-#         'username': username.String,
-#         'name': name.String,
-#     })
-    
-# @api.marshal_with(model, envelope='resource')
-# def get(self, **kwargs):
-#     return Users.query.all()
+class DrawerApi(Resource):
+    def get(self, id):
+        return {'hello': 'world'}
 
-# @app.teardown_appcontext
-# def shutdown_session(exception=None):
-    # db_session.remove()
+    def put(self, id):
+        return {'hello': 'world'}
 
+    def delete(self, id):
+        return {'hello': 'world'}
 
-    if __name__ == '__main__':
-        app.run(debug=True)
+api.add_resource(UserListApi, '/api/v0/users', endpoint = 'users')
+api.add_resource(UserApi, '/api/v0/user/<int:id>', endpoint = 'user')
+
+api.add_resource(ClassListApi, '/api/v0/classes', endpoint = 'classes')
+api.add_resource(ClassApi, '/api/v0/class/<int:id>', endpoint = 'class')
+
+api.add_resource(FamilyListApi, '/api/v0/families', endpoint = 'families')
+api.add_resource(FamilyApi, '/api/v0/family/<int:id>', endpoint = 'family')
+
+api.add_resource(AgeListApi, '/api/v0/ages', endpoint = 'ages')
+api.add_resource(AgeApi, '/api/v0/age/<int:id>', endpoint = 'age')
+
+api.add_resource(AcquisitionListApi, '/api/v0/acquisitions', endpoint = 'acquisitions')
+api.add_resource(AcquisitionApi, '/api/v0/acquisition/<int:id>', endpoint = 'acquisition')
+
+api.add_resource(SpecimenListApi, '/api/v0/specimens', endpoint = 'specimens')
+api.add_resource(SpecimenApi, '/api/v0/specimen/<int:id>', endpoint = 'specimen')
+
+api.add_resource(MapListApi, '/api/v0/maps', endpoint = 'maps')
+api.add_resource(MapApi, '/api/v0/map/<int:id>', endpoint = 'map')
+
+api.add_resource(CollectionListApi, '/api/v0/collections', endpoint = 'collections')
+api.add_resource(CollectionApi, '/api/v0/collection/<int:id>', endpoint = 'collection')
+
+api.add_resource(ScaleListApi, '/api/v0/scales', endpoint = 'scales')
+api.add_resource(ScaleApi, '/api/v0/scale/<int:id>', endpoint = 'scale')
+
+api.add_resource(IslandListApi, '/api/v0/islands', endpoint = 'islands')
+api.add_resource(IslandApi, '/api/v0/island/<int:id>', endpoint = 'island')
+
+api.add_resource(RackListApi, '/api/v0/racks', endpoint = 'racks')
+api.add_resource(RackApi, '/api/v0/rack/<int:id>', endpoint = 'rack')
+
+api.add_resource(DrawerListApi, '/api/v0/drawers', endpoint = 'drawers')
+api.add_resource(DrawerApi, '/api/v0/drawer/<int:id>', endpoint = 'drawer')
+
+if __name__ == '__main__':
+    app.run(debug=True)
